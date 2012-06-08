ab7f834570119f451d65b52f1dfa5a041e09d79d
Deutsch Thibault <thibault.deutsch@gmail.com>
2012-06-09 00:57:51 +0200

    Déplacement de setup.py dans le dossier SOURCE

--- Package/0.1/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-"""A utiliser avec la commande ci-dessous dans le même dossier que main.py :
-python setupe.py py2exe
-"""
-
-setup(console = ["main.py"], description = "Simulateur de clique molette", version = "0.1")
---
+++ Source/setup.py
@@ -0,0 +1,20 @@
+﻿# -*-coding:Utf-8 -*
+
+from distutils.core import setup
+import py2exe
+
+"""A utiliser avec la commande ci-dessous dans le même dossier que main.py :
+python setup.py py2exe
+"""
+
+setup(name="ClickMolette", 
+      version="0.1.5", 
+      description="Simulateur de clique molette", 
+      author="Deutsch Thibault", 
+      author_email="thibault.deutsch@gmail.com", 
+      windows = [
+        {
+            "script": "main.py",
+            "icon_resources": [(1, "icone.ico")]
+        }
+    ],)
